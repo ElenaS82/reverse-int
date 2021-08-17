3a7191c35a6901510c46553bdb1d7a259ed977ad
ElenaS82 <sergeevaelenavl@gmail.com>
2021-08-17 20:57:20 +0300

    feat: add main script

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let strings = String(n);
+  let arr = strings.split(""); 
+  let arr1 = arr.reverse("");
+  let strings2 = arr1.join("");
+  let numberReverse = +  strings2;
+   return numberReverse; 
+ }  
 }
